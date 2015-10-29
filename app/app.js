883c8d94a28e3f4e581bfca56f7470289c7b6b90
Skyvory <skyvory@hotmail.com>
2015-10-29 16:47:12 +0800

    add initial functionality according example

---
+++ app/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var app = angular.module('retractionApp', [
+	'ui.router',
+	'ngSanitize',
+	'ngTwitter',
+	'retractionApp.services',
+]);
+
+// $twitterApi.configure(clientId, )
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	// redirect to the auth state if any other states are requested other than users
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider
+		// .state('auth', {
+		// 	url: '/auth',
+		// 	templateUrl: 'views/authView.html',
+		// 	controller: 'AuthController as auth',
+		// })
+		.state('common', {
+			templateUrl: 'views/common.html',
+			abstract: true,
+		})
+		.state('home', {
+			url: '/home',
+			templateUrl: 'views/homeView.html',
+			controller: 'HomeController as home',
+			parent: 'common',
+		})
+		// .state('logout', {
+		// 	url: '/logout',
+		// 	// templateUrl: 'views/logoutView',
+		// 	controller: 'LogoutController',
+		// });
+}]);
---
+++ app/controllers/authController.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('retractionApp')
+		.controller('AuthController', AuthController);
+
+	function AuthController($state, $scope) {
+
+
+		
+
+	}
+})();
